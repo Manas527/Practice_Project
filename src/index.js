dba479dbe9ca19b0a179dc35d839eff70d17f528
Manas527 <manasgupta88875@gmail.com>
2025-06-25 13:54:20 +0530

    Implement user registration route and async error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import {app} from './app.js';
 
 dotenv.config(
     { path: './config/.env' }
 )
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000} and connected to database ${process.env.DB_NAME}`);
+    })
+})
+.catch((err) => {
+    console.log('MongoDB connection failed ', err);
+})
 
 
 /*
